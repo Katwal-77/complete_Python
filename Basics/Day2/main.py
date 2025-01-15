25ac1a3176406a93f4a8204f46ebcf67deee5646
Prem Katwal <premkatwal987@gmail.com>
2025-01-15 15:07:20 +0800

    added main file inside the day two

---
+++ Basics/Day2/main.py
@@ -0,0 +1,10 @@
+# string concatenation 
+first_name = "prem"
+last_name = "katuwal"
+full_name = first_name + " " + last_name
+print("full name is ", full_name)
+
+first_name = input("Enter your first Name: ")
+last_name = input("Enter your Last Name:")
+full_name = first_name + " " + last_name
+print("full Name is ", full_name)
--- Basics/Day2/strings_input_output.py
+++ Basics/Day2/strings_input_output.py
@@ -2,3 +2,20 @@
 name = input("Enter your name :")
 age = input("Enter you age :")
 print("hello", name.capitalize(), "you are", age, "years old!")
+
+# user aske for favourite color 
+color = input("Enter your favorite color: ")
+print("Your favourite color is", color.upper())
+
+# casting data type
+
+# convert integer to string
+num = 10 
+print ("num is of  type", type(num))
+
+num = 4
+num1 = str(num)
+print ("the value of num is converted to string", num1)
+
+x = 10
+y = str(x)
